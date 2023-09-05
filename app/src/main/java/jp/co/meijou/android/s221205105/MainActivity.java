d5316436be6d4882295b2c7b84e50f5995b19759
Kaito-Tera <221205105@ccmailg.meijo-u.ac.jp>
2023-09-05 14:47:39 +0900

    changed PredataStore

--- app/src/main/java/jp/co/meijou/android/s221205105/MainActivity.java
+++ app/src/main/java/jp/co/meijou/android/s221205105/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import jp.co.meijou.android.s221205105.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        prefDataStore = PrefDataStore.getInstance(this);
+
+
+
+
+        binding.saveButton.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+            prefDataStore.setString("name", text);
+        });
+
     }
+
 }
