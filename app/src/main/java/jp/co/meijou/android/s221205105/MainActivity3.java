681e3fede518c1afad04a9c93a10907ad56296b7
Kaito-Tera <221205105@ccmailg.meijo-u.ac.jp>
2023-09-06 15:31:17 +0900

    add subActivity action

--- app/src/main/java/jp/co/meijou/android/s221205105/MainActivity3.java
+++ app/src/main/java/jp/co/meijou/android/s221205105/MainActivity3.java
@@ -3,6 +3,7 @@ package jp.co.meijou.android.s221205105;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import jp.co.meijou.android.s221205105.databinding.ActivityMain3Binding;
@@ -19,6 +20,21 @@ public class MainActivity3 extends AppCompatActivity {
 
         binding.mbutton.setOnClickListener(view -> {
             var intent = new Intent(this,SubActivity.class);
+            startActivity(intent);
+
+        });
+        binding.abutton.setOnClickListener(view -> {
+            var intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.yahoo.co.jp"));
+
+            startActivity(intent);
+
+        });
+
+        binding.button1.setOnClickListener(view -> {
+            var intent = new Intent(this,SubActivity.class);
+            intent.putExtra("text",binding.editText.getText().toString());
             startActivity(intent);
 
         });
--- app/src/main/java/jp/co/meijou/android/s221205105/SubActivity.java
+++ app/src/main/java/jp/co/meijou/android/s221205105/SubActivity.java
@@ -2,7 +2,10 @@ package jp.co.meijou.android.s221205105;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Optional;
 
 import jp.co.meijou.android.s221205105.databinding.ActivityMain3Binding;
 import jp.co.meijou.android.s221205105.databinding.ActivitySubBinding;
@@ -15,6 +18,9 @@ public class SubActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivitySubBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Optional.ofNullable(getIntent().getStringExtra("text"))
+                .ifPresent(text -> binding.textView2.setText(text));
 
 
     }
